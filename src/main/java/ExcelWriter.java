95dd654d4d53d9f6a48c6e2ede26e007933cf072
yamamotodin <yamamotodin@gmail.com>
2018-07-24 16:57:58 +0900

    merge to a file.

--- src/main/java/ExcelWriter.java
+++ src/main/java/ExcelWriter.java
@@ -16,11 +16,16 @@ import java.util.stream.Stream;
 
 public class ExcelWriter {
 
-    public static final String RECEIPT_TEMPLATE_ORIG_XLSX_FILE_PATH = "./receipt_template_original.xlsx";
-    public static final String RECEIPT_TEMPLATE_XLSX_FILE_PATH = "./receipt_template.xlsx";
-    public static final String RECEIPT_OUTPUT_XLSX_FILE_PATH = "./receipt_output.xlsx";
+    private static final String RECEIPT_TEMPLATE_ORIG_XLSX_FILE_PATH = "./receipt_template_original.xlsx";
+    private static final String RECEIPT_TEMPLATE_XLSX_FILE_PATH = "./receipt_template.xlsx";
+    private static final String RECEIPT_OUTPUT_XLSX_FILE_PATH = "./receipt_output.xlsx";
 
-    static final Receipt sample = new Receipt(
+    // for macro, xlsm file
+    //    private static final String RECEIPT_TEMPLATE_ORIG_XLSX_FILE_PATH = "./receipt_template_macro_original.xlsm";
+    //    private static final String RECEIPT_TEMPLATE_XLSX_FILE_PATH = "./receipt_template_macro.xlsm";
+    //    private static final String RECEIPT_OUTPUT_XLSX_FILE_PATH = "./receipt_output_macro.xlsm";
+
+    private static final Receipt sample = new Receipt(
             "10002-0010005",
             "2016年7月23日",
             "株式会社今北産業",
@@ -36,7 +41,7 @@ public class ExcelWriter {
             "ショッピングパークカスタマーセンター",
             "TEL：03-1111-2222");
 
-    public static final void main(String argv[]) throws IOException, InvalidFormatException {
+    public static void main() throws IOException, InvalidFormatException {
         modify(new File(RECEIPT_TEMPLATE_ORIG_XLSX_FILE_PATH), new File(RECEIPT_OUTPUT_XLSX_FILE_PATH), sample);
     }
 
@@ -46,63 +51,59 @@ public class ExcelWriter {
         Path templatePath = java.nio.file.Files.copy(FileSystems.getDefault().getPath(originalTemplate.getPath()), FileSystems.getDefault().getPath(RECEIPT_TEMPLATE_XLSX_FILE_PATH));
         Path outputPath = java.nio.file.Files.copy(FileSystems.getDefault().getPath(originalTemplate.getPath()), FileSystems.getDefault().getPath(output.getPath()));
 
-        Map <Field, String> fieldMethodMap = getMap(bean);
+        Map<Field, String> fieldMethodMap = getMap(bean);
 
         // Obtain a workbook from the excel file
-        Workbook workbook = WorkbookFactory.create(templatePath.toFile());
+        final Workbook workbook = WorkbookFactory.create(templatePath.toFile());
+        try {
+            // Get Sheet at index 0
+            Sheet sheet = workbook.getSheetAt(0);
 
-        // Get Sheet at index 0
-        Sheet sheet = workbook.getSheetAt(0);
-
-        for (Field field : fieldMethodMap.keySet()){
-            String fieldName = field.getName();
-            if (field.getAnnotation(Direction.class)!=null){
-                fieldName = field.getAnnotation(Direction.class).column();
-            }
-            findCell(sheet, "${"+fieldName+"}").ifPresent(cell -> {
-                cell.setCellType(CellType.STRING);
-                cell.setCellValue(fieldMethodMap.get(field));
+            fieldMethodMap.keySet().forEach(field -> {
+                String fieldName = field.getName();
+                if (field.getAnnotation(Direction.class) != null) {
+                    fieldName = field.getAnnotation(Direction.class).column();
+                }
+                findCell(sheet, "${" + fieldName + "}").ifPresent(cell -> {
+                    cell.setCellType(CellType.STRING);
+                    cell.setCellValue(fieldMethodMap.get(field));
+                });
             });
+
+            // Update the cell's value
+            // Write the output to a file
+            FileOutputStream fileOut = new FileOutputStream(outputPath.toFile());
+            workbook.write(fileOut);
+            fileOut.close();
+
+        } finally {
+            // Closing the workbook
+            workbook.close();
         }
-
-        // Update the cell's value
-        // Write the output to a file
-        FileOutputStream fileOut = new FileOutputStream(outputPath.toFile());
-        workbook.write(fileOut);
-        fileOut.close();
-
-        // Closing the workbook
-        workbook.close();
     }
 
     private static Map<Field, String> getMap(Object bean) { // fieldName, value
         final Map<Field, String> map = new HashMap<>();
-        Stream.of(bean.getClass().getDeclaredFields()).forEach(field -> {
-            Stream.of(bean.getClass().getMethods()).filter( method -> {
-                return method.getName().toLowerCase().equals("get"+field.getName().toLowerCase());
-            }).findFirst().ifPresent(method -> {
-                try {
-                    Object o = method.invoke(bean);
-                    if (o!=null) {
-                        map.put(field, o.toString());
-                    }
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
+        Stream.of(bean.getClass().getDeclaredFields()).forEach(field -> Stream.of(bean.getClass().getMethods()).filter(method -> {
+            return method.getName().equalsIgnoreCase("get" + field.getName().toLowerCase());
+        }).findFirst().ifPresent(method -> {
+            try {
+                Object o = method.invoke(bean);
+                if (o != null) {
+                    map.put(field, o.toString());
                 }
-            });
-        });
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }));
         return map;
     }
 
     private static Optional<Cell> findCell(Sheet sheet, String cellContent) {
         for (Row row : sheet) {
             for (Cell cell : row) {
-                if (cell.getCellTypeEnum() == CellType.STRING) {
-                    if (cell.getRichStringCellValue().getString().trim().equals(cellContent)) {
-                        return Optional.of(cell);
-                    }
+                if (cell.getCellTypeEnum() == CellType.STRING && cell.getRichStringCellValue().getString().trim().equals(cellContent)) {
+                    return Optional.of(cell);
                 }
             }
         }
